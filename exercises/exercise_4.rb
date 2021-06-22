4f7175e478a6e9e5d70435fa6d2b34ddc2a40619
tandica <williams.tandica@gmail.com>
2021-06-22 11:03:56 -0400

    did ex4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,35 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+store4 = Store.create do |x|
+  x.name = "Surrey"
+  x.annual_revenue = 224000
+  x.mens_apparel = false
+  x.womens_apparel = true
+end
+
+store4 = Store.create do |x|
+  x.name = "Whistler"
+  x.annual_revenue = 1900000
+  x.mens_apparel = true
+  x.womens_apparel = false
+end
+
+store6 = Store.create do |x|
+  x.name = "Yaletown"
+  x.annual_revenue = 430000
+  x.mens_apparel = true
+  x.womens_apparel = true
+end
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel:true)
+ @mens_stores.each do |y| 
+  p [y.name , y.annual_revenue]
+end
+
+@womens_stores = Store.having('(annual_revenue)< 1000000').group('id','womens_apparel')
+ @womens_stores.each do |y|
+  p [y.name, y.annual_revenue]
+ end
