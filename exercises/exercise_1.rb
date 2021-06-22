e14e0a096d67b80a9cfe8956a09dd3854b992850
tandica <williams.tandica@gmail.com>
2021-06-22 10:51:18 -0400

    did ex1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,25 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+store1 = Store.create do |x|
+  x.name = "Burnaby"
+  x.annual_revenue = 300000
+  x.mens_apparel = true
+  x.womens_apparel = true
+end
+
+store2 = Store.create do |x|
+  x.name = "Richmond"
+  x.annual_revenue = 1260000
+  x.mens_apparel = false
+  x.womens_apparel = true
+end
+
+store3 = Store.create do |x|
+  x.name = "Gastown"
+  x.annual_revenue = 190000
+  x.mens_apparel = true
+  x.womens_apparel = false
+end
+
+puts Store.count
