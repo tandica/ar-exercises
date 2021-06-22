7de2152a08a7def9da3a999e6a112011f23e12ab
tandica <williams.tandica@gmail.com>
2021-06-22 11:10:36 -0400

    did ex6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,15 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store 
+  has_many :employees
+end
+
+class Employee 
+  belongs_to :Store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Tandica", last_name: "Williams", hourly_rate: 2000)
+@store2.employees.create(first_name: "Karim", last_name: "Benzenzen", hourly_rate: 60)
+@store2.employees.create(first_name: "Cindy", last_name: "Crawhonda", hourly_rate: 80)
